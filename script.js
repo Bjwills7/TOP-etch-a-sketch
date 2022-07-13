b266d16bd372e262be18650bd2e60aae141a3992
Bjwills7 <bjwills7@hotmail.com>
2022-07-12 21:16:41 -0500

    Add temp var for anonymous func

--- script.js
+++ script.js
@@ -4,17 +4,23 @@
 
 // addEventListeners to change grid size when buttons are pressed /done
 
+// move pixel array and iterator to global scope and use it to add event listener to each pixel
+
 // add logic to change div colors on hover or click and hold
+
+// problem: call event listener on all divs but function only applies to the div being hovered
+
 
 window.onload = function() {
     grid16();
 }
 
+let pixel = [];
+let i = 0;
 let container = document.querySelector('.container');
 let button16 = document.querySelector('.button16');
 let button32 = document.querySelector('.button32');
 let button64 = document.querySelector('.button64');
-
 
 button16.addEventListener('click', function() {grid16()});
 button32.addEventListener('click', function() {grid32()});
@@ -23,18 +29,18 @@ button64.addEventListener('click', function() {grid64()});
 
 function grid16() {
     reset();
-    let pixel = [];
-    for (let i = 0; i < 256; i++) {
+    for (i = 0; i < 256; i++) {
+        let j = i;
         pixel[i] = document.createElement('div');
         pixel[i].classList.add(`pixel${i}`, `div16`);
         container.appendChild(pixel[i]);
+        pixel[i].addEventListener('mousedown', function() {console.log(pixel[j])});
     }
 }
 
 function grid32() {
     reset();
-    let pixel = [];
-    for (let i = 0; i < 1024; i++) {
+    for (i = 0; i < 1024; i++) {
         pixel[i] = document.createElement('div');
         pixel[i].classList.add(`pixel${i}`, `div32`);
         container.appendChild(pixel[i]);
@@ -43,8 +49,7 @@ function grid32() {
 
 function grid64() {
     reset();
-    let pixel = [];
-    for (let i = 0; i < 4096; i++) {
+    for (i = 0; i < 4096; i++) {
         pixel[i] = document.createElement('div');
         pixel[i].classList.add(`pixel${i}`, `div64`);
         container.appendChild(pixel[i]);
